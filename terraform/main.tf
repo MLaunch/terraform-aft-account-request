de228de7996f39d61090eab3bfb205e5f4dce29c
Matthew <80968027+MLaunch@users.noreply.github.com>
2022-06-02 23:00:08 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,10 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "aws-sandbox-account-test2@gdit.com"
-    AccountName               = "sandbox-aft-test2"
+    AccountEmail              = "aws-sandbox-account-test3@gdit.com"
+    AccountName               = "sandbox-aft-test3"
     ManagedOrganizationalUnit = "Sandbox"
-    SSOUserEmail              = "aws-sandbox-account-test2@gdit.com"
+    SSOUserEmail              = "aws-sandbox-account-test3@gdit.com"
     SSOUserFirstName          = "Sandbox"
     SSOUserLastName           = "Tester"
   }
