788c9d58a7107c67ad702f21126a3a519ceaa8c2
jeffrey-dillGT <107069096+jeffrey-dillGT@users.noreply.github.com>
2022-07-21 15:12:09 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,27 +1,27 @@
-# module "sandbox" {
-#  source = "./modules/aft-account-request"
+module "sandbox" {
+ source = "./modules/aft-account-request"
 
-#  control_tower_parameters = {
-#    AccountEmail              = "aws-sandbox-account-test1@gdit.com"
-#    AccountName               = "sandbox-aft-test1"
-#    ManagedOrganizationalUnit = "Sandbox"
-#    SSOUserEmail              = "aws-sandbox-account-test1@gdit.com"
-#    SSOUserFirstName          = "Sandbox"
-#    SSOUserLastName           = "Tester"
-#  }
+ control_tower_parameters = {
+   AccountEmail              = "aws-sandbox-account-test1@gdit.com"
+   AccountName               = "sandbox-aft-test1"
+   ManagedOrganizationalUnit = "Sandbox"
+   SSOUserEmail              = "aws-sandbox-account-test1@gdit.com"
+   SSOUserFirstName          = "Sandbox"
+   SSOUserLastName           = "Tester"
+ }
 
-#  account_tags = {
-#    "Team" = "Cloud Growth"
-#  }
+ account_tags = {
+   "Team" = "Cloud Growth"
+ }
 
- # change_management_parameters = {
- #   change_requested_by = "Master Chief"
- #   change_reason       = "Automation Control Command"
- # }
+ change_management_parameters = {
+   change_requested_by = "Master Chief"
+   change_reason       = "Automation Control Command"
+ }
 
- # custom_fields = {
- #   group = "non-prod"
- # }
+ custom_fields = {
+   group = "non-prod"
+ }
 
- # account_customizations_name = "sandbox"
-#}
+ account_customizations_name = "sandbox"
+}
