8a7400d4e8c6c337aef713d04e3e38a2b7a3105b
jeffrey-dillGT <107069096+jeffrey-dillGT@users.noreply.github.com>
2022-08-17 16:09:04 -0400

    Update main.tf
    remove test account 4

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,31 +1,3 @@
-module "sandbox4" {
- source = "./modules/aft-account-request"
-
- control_tower_parameters = {
-   AccountEmail              = "aws-sandbox-account-test4@gdit.com"
-   AccountName               = "sandbox-aft-test4"
-   ManagedOrganizationalUnit = "Sandbox"
-   SSOUserEmail              = "aws-sandbox-account-test4@gdit.com"
-   SSOUserFirstName          = "Sandbox"
-   SSOUserLastName           = "Tester1"
- }
-
- account_tags = {
-   "Team" = "Cloud Growth"
- }
-
- change_management_parameters = {
-   change_requested_by = "Master Chief"
-   change_reason       = "Automation Control Command"
- }
-
- custom_fields = {
-   group = "non-prod"
- }
-
- account_customizations_name = "sandbox"
-}
-
 module "sandbox1" {
  source = "./modules/aft-account-request"
 
