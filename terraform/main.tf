92cf654e38b59a715e380e7e752285b0cd93972d
Matthew <80968027+MLaunch@users.noreply.github.com>
2022-08-17 13:58:35 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,3 +25,31 @@ module "sandbox4" {
 
  account_customizations_name = "sandbox"
 }
+
+ module "sandbox3" {
+ source = "./modules/aft-account-request"
+
+ control_tower_parameters = {
+   AccountEmail              = "aws-sandbox-account-test3@gdit.com"
+   AccountName               = "sandbox-aft-test3"
+   ManagedOrganizationalUnit = "Sandbox"
+   SSOUserEmail              = "aws-sandbox-account-test3@gdit.com"
+   SSOUserFirstName          = "Sandbox"
+   SSOUserLastName           = "Tester3"
+ }
+
+ account_tags = {
+   "Team" = "Cloud Growth"
+ }
+
+ change_management_parameters = {
+   change_requested_by = "Master Chief"
+   change_reason       = "Automation Control Command"
+ }
+
+ custom_fields = {
+   group = "non-prod"
+ }
+
+ account_customizations_name = "sandbox"
+}
